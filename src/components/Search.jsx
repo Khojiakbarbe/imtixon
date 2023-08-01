3b29bcdb331ee3ec2264acd6026026d462e8af33
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-08-02 01:17:56 +0500

    responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -24,7 +24,7 @@ function Search() {
     return (
         <div className='p-5 '>
             <form onSubmit={submit} className='flex items-start text-[10px] sm:text-xs md:text-base lg:text-lg'>
-                <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder='search' className='bg-[#5A5C66] text-white outline-none p-2 md:px-5 md:py-3' />
+                <input value={value} onChange={(e) => setValue(e.target.value)} type="text" placeholder='search' className='bg-[#5A5C66] w-[50%] text-white outline-none p-2 md:px-5 md:py-3' />
                 <button className='bg-[#5A5C66] text-white p-2 md:py-4 md:px-5'><BsSearch /></button>
             </form>
         </div>
